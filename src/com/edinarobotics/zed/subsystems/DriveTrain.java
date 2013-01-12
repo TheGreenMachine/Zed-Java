5e6ecdc6bef742064379aae8787fa9551b573fb1
Eric <congpeng89@gmail.com>
2013-01-12 14:54:45 -0600

    Added DriveTrain subsystem.
    This class will be used to drive the Mechanum DriveTrain style.

---
+++ src/com/edinarobotics/zed/subsystems/DriveTrain.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.edinarobotics.zed.subsystems;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+/**
+ *
+ * @author Eric
+ */
+public class DriveTrain extends Subsystem {
+    
+    public DriveTrain() {
+        
+    }
+    
+    public void initDefaultCommand(){
+        
+    }
+}
