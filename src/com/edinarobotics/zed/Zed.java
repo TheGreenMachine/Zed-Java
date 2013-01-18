5536653e6d004bbbd2504cb5212789568da639fb
Karl Otness <karl@otness.com>
2013-01-18 14:26:32 -0600

    Remove an auto-generated comment from Zed.

--- src/com/edinarobotics/zed/Zed.java
+++ src/com/edinarobotics/zed/Zed.java
@@ -10,14 +10,8 @@ package com.edinarobotics.zed;
 
 import edu.wpi.first.wpilibj.IterativeRobot;
 
-/**
- * The VM is configured to automatically run this class, and to call the
- * functions corresponding to each mode, as described in the IterativeRobot
- * documentation. If you change the name of this class or the package after
- * creating this project, you must also update the manifest file in the resource
- * directory.
- */
 public class Zed extends IterativeRobot {
+    
     /**
      * This function is run when the robot is first started up and should be
      * used for any initialization code.
