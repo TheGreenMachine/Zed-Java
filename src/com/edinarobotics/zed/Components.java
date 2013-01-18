bb34b4a52d2fdb053b66225a2d64e5dcbb40f6a1
Karl Otness <karl@otness.com>
2013-01-18 14:15:46 -0600

    Add Drivetrain to the Components class.
    Drivetrain will now be constructed along with the rest of the robot
    subsystems.

--- src/com/edinarobotics/zed/Components.java
+++ src/com/edinarobotics/zed/Components.java
@@ -1,15 +1,38 @@
 
 package com.edinarobotics.zed;
 
+import com.edinarobotics.zed.subsystems.Drivetrain;
+
 public class Components {
     private static Components instance;
+    
+    //Drivetrain constants
+    private static final int FRONT_LEFT_DRIVE_JAGUAR = 1;
+    private static final int FRONT_RIGHT_DRIVE_JAGUAR = 2;
+    private static final int BACK_LEFT_DRIVE_JAGUAR = 3;
+    private static final int BACK_RIGHT_DRIVE_JAGUAR = 4;
+    
+    //Subsystem objects
+    public final Drivetrain drivetrain;
+    
+    /**
+     * Private constructor for the Components singleton. This constructor
+     * is only called once and handles creating all the robot subsystems.
+     */
+    private Components(){
+        drivetrain = new Drivetrain(FRONT_LEFT_DRIVE_JAGUAR, FRONT_RIGHT_DRIVE_JAGUAR,
+                BACK_LEFT_DRIVE_JAGUAR, BACK_RIGHT_DRIVE_JAGUAR);
+    }
     
     /**
      * Returns a new instance of {@link Components}, creating one if null.
      * @return {@link Components}
      */
     public static Components getInstance() {
-        return instance == null ? instance = new Components() : instance;
+        if(instance == null){
+            instance = new Components();
+        }
+        return instance;
     }
     
 }
