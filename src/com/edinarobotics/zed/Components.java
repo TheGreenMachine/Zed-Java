0eb030e6e4aee15c1a77a1cb4cd5b6bd83087019
Karl Otness <karl@otness.com>
2013-01-18 13:37:51 -0600

    Remove auto-generated comments from the project.

--- src/com/edinarobotics/zed/Components.java
+++ src/com/edinarobotics/zed/Components.java
@@ -1,18 +1,11 @@
 
 package com.edinarobotics.zed;
 
-/**
- *
- * @author Eric
- */
-
 public class Components {
-    
     private static Components instance;
     
-    
     /**
-     * Returns instance of {@link Components}, creating one if null.
+     * Returns a new instance of {@link Components}, creating one if null.
      * @return {@link Components}
      */
     public static Components getInstance() {
--- src/com/edinarobotics/zed/subsystems/Drivetrain.java
+++ src/com/edinarobotics/zed/subsystems/Drivetrain.java
@@ -1,15 +1,7 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.edinarobotics.zed.subsystems;
 
 import edu.wpi.first.wpilibj.command.Subsystem;
 
-/**
- *
- * @author Eric
- */
 public class Drivetrain extends Subsystem {
     
     public Drivetrain() {
