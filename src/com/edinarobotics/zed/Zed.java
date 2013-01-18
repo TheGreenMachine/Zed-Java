77a9157c02c3b4efc483d2b3d9e5ed1b852ab073
Karl Otness <karl@otness.com>
2013-01-18 14:16:11 -0600

    Add call to Components.getInstance to robotInit.
    This call will create all subsystems at robot init time.

--- src/com/edinarobotics/zed/Zed.java
+++ src/com/edinarobotics/zed/Zed.java
@@ -23,7 +23,7 @@ public class Zed extends IterativeRobot {
      * used for any initialization code.
      */
     public void robotInit() {
-
+        Components.getInstance(); //Create all robot subsystems.
     }
 
     /**
