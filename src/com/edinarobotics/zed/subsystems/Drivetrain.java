003bce9f1a443c361020cd77875d3884a62f3a09
Karl Otness <karl@otness.com>
2013-01-18 14:15:20 -0600

    Add mecanum drive definition to Drivetrain.
    Drivetrain is not capable of mecanum control and is also a Subsystem1816
    subclass.

--- src/com/edinarobotics/zed/subsystems/Drivetrain.java
+++ src/com/edinarobotics/zed/subsystems/Drivetrain.java
@@ -1,14 +1,39 @@
 package com.edinarobotics.zed.subsystems;
 
+import com.edinarobotics.utils.commands.MaintainStateCommand;
+import com.edinarobotics.utils.subsystems.Subsystem1816;
+import edu.wpi.first.wpilibj.RobotDrive;
+import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
-public class Drivetrain extends Subsystem {
+public class Drivetrain extends Subsystem1816 {
+    private RobotDrive robotDrive;
+    private double magnitude;
+    private double direction;
+    private double rotation;
     
-    public Drivetrain() {
-        
+    public Drivetrain(int frontLeft, int frontRight, int backLeft, int backRight) {
+        super("Drivetrain");
+        this.robotDrive = new RobotDrive(frontLeft, backLeft, frontRight, backRight);
     }
     
     public void initDefaultCommand(){
-        
+        setDefaultCommand(new MaintainStateCommand(this));
+    }
+    
+    
+    public void setDefaultCommand(Command command){
+        super.setDefaultCommand(command);
+    }
+    
+    public void mecanumPolar(double magnitude, double direction, double rotation){
+        this.magnitude = magnitude;
+        this.direction = direction;
+        this.rotation = rotation;
+        update();
+    }
+    
+    public void update(){
+        robotDrive.mecanumDrive_Polar(magnitude, direction, rotation);
     }
 }
