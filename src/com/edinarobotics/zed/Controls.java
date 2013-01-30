be5808a3e41a8630dc7456f87ec689024c1baa1c
Karl Otness <karl@otness.com>
2013-01-29 20:50:54 -0600

    Add preliminary controls to the gamepad buttons.
    The buttons on the drive gamepad now perform robot actions.

--- src/com/edinarobotics/zed/Controls.java
+++ src/com/edinarobotics/zed/Controls.java
@@ -1,6 +1,9 @@
 package com.edinarobotics.zed;
 
 import com.edinarobotics.utils.gamepad.Gamepad;
+import com.edinarobotics.zed.commands.*;
+import com.edinarobotics.zed.subsystems.Collector;
+import com.edinarobotics.zed.subsystems.Conveyor;
 
 /**
  * Controls handles creating the {@link Gamepad} objects
@@ -9,12 +12,33 @@ import com.edinarobotics.utils.gamepad.Gamepad;
  */
 public class Controls {
     private static Controls instance;
+    private static final double ONE_JOYSTICK_MAGNITUDE = 1;
     
     public final Gamepad gamepad1;
     public final Gamepad gamepad2;
     
     private Controls(){
         gamepad1 = new Gamepad(1);
+        //Conveyor
+        gamepad1.LEFT_TRIGGER.whenPressed(new SetConveyorCommand(Conveyor.CONVEYOR_IN));
+        gamepad1.LEFT_TRIGGER.whenReleased(new SetConveyorCommand(Conveyor.CONVEYOR_STOP));
+        gamepad1.LEFT_BUMPER.whenPressed(new SetConveyorCommand(Conveyor.CONVEYOR_OUT));
+        gamepad1.LEFT_BUMPER.whenReleased(new SetConveyorCommand(Conveyor.CONVEYOR_STOP));
+        //Collector
+        gamepad1.RIGHT_TRIGGER.whenPressed(new SetCollectorCommand(Collector.COLLECTOR_IN));
+        gamepad1.RIGHT_TRIGGER.whenReleased(new SetCollectorCommand(Collector.COLLECTOR_STOP));
+        gamepad1.RIGHT_BUMPER.whenPressed(new SetCollectorCommand(Collector.COLLECTOR_OUT));
+        gamepad1.RIGHT_BUMPER.whenReleased(new SetCollectorCommand(Collector.COLLECTOR_STOP));
+        //One-Joystick Strafe
+        gamepad1.DPAD_UP.whileHeld(new SetDrivetrainCommand(ONE_JOYSTICK_MAGNITUDE, 0, 0));
+        gamepad1.DPAD_UP.whenReleased(new SetDrivetrainCommand(0, 0, 0));
+        gamepad1.DPAD_DOWN.whileHeld(new SetDrivetrainCommand(ONE_JOYSTICK_MAGNITUDE, 180, 0));
+        gamepad1.DPAD_DOWN.whenReleased(new SetDrivetrainCommand(0, 0, 0));
+        gamepad1.DPAD_LEFT.whileHeld(new SetDrivetrainCommand(ONE_JOYSTICK_MAGNITUDE, -90, 0));
+        gamepad1.DPAD_LEFT.whenReleased(new SetDrivetrainCommand(0, 0, 0));
+        gamepad1.DPAD_RIGHT.whileHeld(new SetDrivetrainCommand(ONE_JOYSTICK_MAGNITUDE, 90, 0));
+        gamepad1.DPAD_RIGHT.whenReleased(new SetDrivetrainCommand(0, 0, 0));
+        
         gamepad2 = new Gamepad(2);
     }
     
