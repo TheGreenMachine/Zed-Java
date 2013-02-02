918fc7cc8566780f3da020f2786ece1da5dd17bc
Srujun Gupta <theununtrium@gmail.com>
2013-02-02 14:47:55 -0600

    Add control of the robot rotation to VisionTrackingCommand.
    VisionTrackingCommand should now control the robot's rotation to
    horizontally center the target on the camera frame.
    The command does not vertically center the target and will throw a
    NullPointerException.

--- src/com/edinarobotics/zed/commands/VisionTrackingCommand.java
+++ src/com/edinarobotics/zed/commands/VisionTrackingCommand.java
@@ -4,14 +4,20 @@ import com.edinarobotics.utils.pid.PIDConfig;
 import com.edinarobotics.utils.pid.PIDTuningManager;
 import com.edinarobotics.zed.Components;
 import com.edinarobotics.zed.subsystems.DrivetrainRotation;
+import com.edinarobotics.zed.subsystems.Lifter;
 import com.edinarobotics.zed.vision.PIDTargetX;
 import com.edinarobotics.zed.vision.PIDTargetY;
+import com.edinarobotics.zed.vision.Target;
 import com.edinarobotics.zed.vision.TargetCollection;
 import edu.wpi.first.wpilibj.PIDController;
 import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.networktables.NetworkTable;
 
 public class VisionTrackingCommand extends Command {
+    public static final byte HIGH_GOAL = 1;
+    public static final byte MIDDLE_GOAL = 2;
+    public static final byte ANY_GOAL = 3;
+    
     private NetworkTable visionTable = NetworkTable.getTable("vision");
     private TargetCollection targetCollection;
     private PIDController pidControllerX;
@@ -19,8 +25,12 @@ public class VisionTrackingCommand extends Command {
     private PIDTargetX pidTargetX;
     private PIDTargetY pidTargetY;
     private DrivetrainRotation drivetrainRotation;
+    private Lifter lifter;
     private PIDConfig xPIDConfig;
     private PIDConfig yPIDConfig;
+    private byte goalType;
+    private double xSetpoint;
+    private double ySetpoint;
     
     private final double X_P = 1;
     private final double X_I = 0;
@@ -30,13 +40,34 @@ public class VisionTrackingCommand extends Command {
     private final double Y_D = 1;
     
     public VisionTrackingCommand() {
+        this(ANY_GOAL);
+    }
+    
+    public VisionTrackingCommand(double timeoutSeconds) {
+        this();
+        setTimeout(timeoutSeconds);
+    }
+    
+    public VisionTrackingCommand(byte goalType, double timeoutSeconds) {
+        this(goalType);
+        setTimeout(timeoutSeconds);
+    }
+    
+    public VisionTrackingCommand(byte goalType) {
+        super("VisionTracking");
+        this.goalType = goalType;
         drivetrainRotation = Components.getInstance().drivetrainRotation;
+        lifter = Components.getInstance().lifter;
         pidTargetX = new PIDTargetX();
         pidTargetY = new PIDTargetY();
         pidControllerX = new PIDController(X_P, X_I, X_D, pidTargetX, drivetrainRotation);
-        pidControllerY = new PIDController(Y_P, Y_I, Y_D, pidTargetY, drivetrainRotation);
+        pidControllerY = new PIDController(Y_P, Y_I, Y_D, pidTargetY, null);
         xPIDConfig = PIDTuningManager.getInstance().getPIDConfig("Vision Horizontal");
         yPIDConfig = PIDTuningManager.getInstance().getPIDConfig("Vision Vertical");
+        xSetpoint = 0;
+        ySetpoint = 0;
+        requires(drivetrainRotation);
+        requires(lifter);
     }
     
     protected void initialize() {
@@ -47,8 +78,18 @@ public class VisionTrackingCommand extends Command {
 
     protected void execute() {
         targetCollection = new TargetCollection(visionTable.getString("vtdata"));
-        pidControllerX.setSetpoint(0);
-        pidControllerY.setSetpoint(0);
+        pidControllerX.setSetpoint(xSetpoint);
+        pidControllerY.setSetpoint(ySetpoint);
+        Target target;
+        if(goalType == HIGH_GOAL) {
+            target = targetCollection.getClosestTarget(xSetpoint, ySetpoint, true);
+        } else if(goalType == MIDDLE_GOAL) {
+            target = targetCollection.getClosestTarget(xSetpoint, ySetpoint, false);
+        } else {
+            target = targetCollection.getClosestTarget(xSetpoint, ySetpoint);
+        }
+        pidTargetX.setTarget(target);
+        pidTargetY.setTarget(target);
         //PID tuning code
         pidControllerX.setPID(xPIDConfig.getP(X_P), xPIDConfig.getI(X_I), xPIDConfig.getD(X_D));
         xPIDConfig.setSetpoint(pidControllerX.getSetpoint());
@@ -59,7 +100,8 @@ public class VisionTrackingCommand extends Command {
     }
 
     protected boolean isFinished() {
-        return false;
+        return (pidControllerX.onTarget() && pidControllerY.onTarget()) ||
+                isTimedOut();
     }
 
     protected void end() {
