e34b370d8a4857201012cf818f9bb62365b02cf7
Srujun Gupta <theununtrium@gmail.com>
2013-02-01 18:03:37 -0600

    Added SetAugerCommand.
    The command will set the direction of the auger's motor.

---
+++ src/com/edinarobotics/zed/commands/SetAugerCommand.java
@@ -0,0 +1,35 @@
+package com.edinarobotics.zed.commands;
+
+import com.edinarobotics.zed.Components;
+import com.edinarobotics.zed.subsystems.Auger;
+import edu.wpi.first.wpilibj.command.Command;
+
+public class SetAugerCommand extends Command {
+    private Auger auger;
+    private byte direction;
+    
+    public SetAugerCommand(byte direction) {
+        super("SetAuger");
+        this.auger = Components.getInstance().auger;
+        this.direction = direction;
+        requires(auger);
+    }
+
+    protected void initialize() {
+        auger.setAugerDirection(direction);
+    }
+
+    protected void execute() {
+    }
+
+    protected boolean isFinished() {
+        return true;
+    }
+
+    protected void end() {
+    }
+
+    protected void interrupted() {
+    }
+    
+}
