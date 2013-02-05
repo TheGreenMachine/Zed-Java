2b0fd208a333a4e57441c49aa1022a3ee856f3bb
Karl Otness <karl@otness.com>
2013-02-05 16:41:37 -0600

    Update naming of constants in Components.
    Components is now up to date with the robot's electronics
    configuration.

--- src/com/edinarobotics/zed/Components.java
+++ src/com/edinarobotics/zed/Components.java
@@ -18,21 +18,23 @@ public class Components {
     private static final int FRONT_RIGHT_DRIVE_JAGUAR = 1;
     private static final int BACK_LEFT_DRIVE_JAGUAR = 2;
     private static final int BACK_RIGHT_DRIVE_JAGUAR = 4;
-    //Collector constants
-    private static final int COLLECTOR_LEFT_STAR_SPIKE = 5;
-    private static final int COLLECTOR_RIGHT_STAR_SPIKE = 6;
-    private static final int COLLECTOR_ROLLER_SPIKE = 7;
     //Conveyor constants
     private static final int CONVEYOR_VICTOR = 8;
-    //Auger constants
-    private static final int AUGER_SPIKE = 9;
     
     //CAN constants
     //Shooter constants
     private static final int SHOOTER_JAGUAR_FIRST = 1;
     private static final int SHOOTER_JAGUAR_SECOND = 2;
+    
+    //Relay constants
+    //Auger constants
+    private static final int AUGER_SPIKE = 1;
     //Lifter constants
-    private static final int LIFTER_JAGUAR = 3;
+    private static final int LIFTER_SPIKE = 2;
+    //Collector constants
+    private static final int COLLECTOR_LEFT_STAR_SPIKE = 3;
+    private static final int COLLECTOR_RIGHT_STAR_SPIKE = 4;
+    private static final int COLLECTOR_LIFTER_SPIKE = 5;
     
     //Subsystem objects
     public final Drivetrain drivetrain;
@@ -54,9 +56,9 @@ public class Components {
         drivetrainStrafe = new DrivetrainStrafe(drivetrain);
         drivetrainRotation = new DrivetrainRotation(drivetrain);
         shooter = new Shooter(SHOOTER_JAGUAR_FIRST, SHOOTER_JAGUAR_SECOND);
-        lifter = new Lifter(LIFTER_JAGUAR);
+        lifter = new Lifter(LIFTER_SPIKE);
         collector = new Collector(COLLECTOR_LEFT_STAR_SPIKE, COLLECTOR_RIGHT_STAR_SPIKE,
-                COLLECTOR_ROLLER_SPIKE);
+                COLLECTOR_LIFTER_SPIKE);
         conveyor = new Conveyor(CONVEYOR_VICTOR);
         auger = new Auger(AUGER_SPIKE);
     }
