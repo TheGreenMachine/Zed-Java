fea5b3e24cf899180bd1cee76a2ac4220192dc89
Srujun Gupta <theununtrium@gmail.com>
2013-02-09 11:45:04 -0600

    Added toString(), equals() and hashCode() methods to Target class.
    These methods will be used for debugging purposes.

--- src/com/edinarobotics/zed/vision/Target.java
+++ src/com/edinarobotics/zed/vision/Target.java
@@ -29,4 +29,27 @@ public class Target {
         return isCenter;
     }
     
+    public String toString() {
+        return "" + x + " " + y + " " + distance + " " + isCenter;
+    }
+
+    public int hashCode() {
+        int hash = 3;
+        hash = 47 * hash + (int) (Double.doubleToLongBits(this.x) ^ (Double.doubleToLongBits(this.x) >>> 32));
+        hash = 47 * hash + (int) (Double.doubleToLongBits(this.y) ^ (Double.doubleToLongBits(this.y) >>> 32));
+        hash = 47 * hash + (int) (Double.doubleToLongBits(this.distance) ^ (Double.doubleToLongBits(this.distance) >>> 32));
+        hash = 47 * hash + (this.isCenter ? 1 : 0);
+        return hash;
+    }
+    
+    public boolean equals(Object other) {
+        if(other instanceof Target) {
+            return (this.x == (double)((Target)other).getX()) &&
+                   (this.y == (double)((Target)other).getY()) &&
+                   (this.distance == (double)((Target)other).getDistance()) &&
+                   (this.isCenter == (boolean)((Target)other).isCenter());
+        }
+        return false;
+    }
+    
 }
