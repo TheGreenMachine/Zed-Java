bb94dffb3ac4e1a92f79d764913f5924c298d0a7
Srujun Gupta <theununtrium@gmail.com>
2013-02-12 15:49:26 -0600

    Added AugerRotateCommand for frisbee loading/dispensing.
    The command will rotate the Auger exactly one rotation to load or dispense
    a frisbee using the digital input and the counter from the Auger class.

---
+++ src/com/edinarobotics/zed/commands/AugerRotateCommand.java
@@ -0,0 +1,43 @@
+package com.edinarobotics.zed.commands;
+
+import com.edinarobotics.zed.Components;
+import com.edinarobotics.zed.subsystems.Auger;
+import com.edinarobotics.zed.subsystems.Auger.AugerDirection;
+import edu.wpi.first.wpilibj.command.Command;
+
+public class AugerRotateCommand extends Command {
+    private Auger auger;
+    private AugerDirection direction;
+    private int startingCount;
+    
+    public AugerRotateCommand(Auger.AugerDirection direction) {
+        super("AugerRotate");
+        this.auger = Components.getInstance().auger;
+        this.direction = direction;
+        setInterruptible(false);
+        setTimeout(2.0);
+        requires(auger);
+    }
+    
+    protected void initialize() {
+        startingCount = auger.getAugerRotationCount();
+        auger.setAugerDirection(direction);
+    }
+
+    protected void execute() {
+        auger.setAugerDirection(direction);
+    }
+
+    protected boolean isFinished() {
+        return auger.getAugerRotationCount() > startingCount ||
+                isTimedOut() || direction.equals(AugerDirection.AUGER_STOP);
+    }
+
+    protected void end() {
+        auger.setAugerDirection(Auger.AugerDirection.AUGER_STOP);
+    }
+
+    protected void interrupted() {
+    }
+    
+}
