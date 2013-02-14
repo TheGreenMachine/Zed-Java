bcb1140ad73f3ca73f3090b6ae3f988103cdc2ca
Srujun Gupta <theununtrium@gmail.com>
2013-02-13 21:37:54 -0600

    Added Encoder and related methods to Drivetrain.
    The Encoder mounted on the robot's wheels will be used to derive the
    distance the robot has travelled forward, for use in the Autonomous
    period.

--- src/com/edinarobotics/zed/subsystems/Drivetrain.java
+++ src/com/edinarobotics/zed/subsystems/Drivetrain.java
@@ -1,19 +1,41 @@
 package com.edinarobotics.zed.subsystems;
 
+import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.RobotDrive;
 
 public class Drivetrain {
+    private final double DISTANCE_PER_PULSE = 1;
     private RobotDrive robotDrive;
     private double magnitude;
     private double direction;
     private double rotation;
     
-    public Drivetrain(int frontLeft, int frontRight, int backLeft, int backRight) {
+    private Encoder frontLeftEncoder;
+    private Encoder frontRightEncoder;
+    private Encoder backLeftEncoder;
+    private Encoder backRightEncoder;
+    
+    public Drivetrain(int frontLeft, int frontRight, int backLeft, int backRight,
+            int frontLeftA, int frontLeftB, int frontRightA, int frontRightB,
+            int backLeftA, int backLeftB, int backRightA, int backRightB) {
         this.robotDrive = new RobotDrive(frontLeft, backLeft, frontRight, backRight);
         robotDrive.setInvertedMotor(RobotDrive.MotorType.kFrontRight, true);
         robotDrive.setInvertedMotor(RobotDrive.MotorType.kFrontLeft, false);
         robotDrive.setInvertedMotor(RobotDrive.MotorType.kRearRight, true);
         robotDrive.setInvertedMotor(RobotDrive.MotorType.kRearLeft, false);
+        
+        frontLeftEncoder = new Encoder(frontLeftA, frontLeftB);
+        frontRightEncoder = new Encoder(frontRightA, frontRightB);
+        backLeftEncoder = new Encoder(backLeftA, backLeftB);
+        backRightEncoder = new Encoder(backRightA, backRightB);
+        frontLeftEncoder.setDistancePerPulse(DISTANCE_PER_PULSE);
+        frontRightEncoder.setDistancePerPulse(DISTANCE_PER_PULSE);
+        backLeftEncoder.setDistancePerPulse(DISTANCE_PER_PULSE);
+        backRightEncoder.setDistancePerPulse(DISTANCE_PER_PULSE);
+        frontLeftEncoder.start();
+        frontRightEncoder.start();
+        backLeftEncoder.start();
+        backRightEncoder.start();
     }
     
     public void mecanumPolarStrafe(double magnitude, double direction){
@@ -29,5 +51,26 @@ public class Drivetrain {
     
     public void update(){
         robotDrive.mecanumDrive_Polar(magnitude, direction, rotation);
+    }
+    
+    public double getFrontLeftVelocity() {
+        return frontLeftEncoder.getRate();
+    }
+    
+    public double getFrontRightVelocity() {
+        return frontRightEncoder.getRate();
+    }
+    
+    public double getBackLeftVelocity() {
+        return backLeftEncoder.getRate();
+    }
+    
+    public double getBackRightVelocity() {
+        return backRightEncoder.getRate();
+    }
+    
+    public double getAverageVelocity() {
+        return 0.5 * ((getFrontLeftVelocity() + getFrontRightVelocity() +
+                getBackLeftVelocity() + getBackRightVelocity()) / 4);
     }
 }
