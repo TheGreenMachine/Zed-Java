0630a96d98bd5b2f372b913dc8ad6f38e191eba2
Srujun Gupta <theununtrium@gmail.com>
2013-02-16 12:08:51 -0600

    Remove PID control from shooter CANJaguars.
    The shooter has no encoders mounted and is now controlled in PercentVBus
    mode, similar to PWM values.

--- src/com/edinarobotics/zed/subsystems/Shooter.java
+++ src/com/edinarobotics/zed/subsystems/Shooter.java
@@ -1,25 +1,13 @@
 package com.edinarobotics.zed.subsystems;
 
-import com.edinarobotics.utils.pid.PIDConfig;
-import com.edinarobotics.utils.pid.PIDTuningManager;
 import com.edinarobotics.utils.subsystems.Subsystem1816;
 import edu.wpi.first.wpilibj.CANJaguar;
 
 public class Shooter extends Subsystem1816 {
-    public static final double SHOOTER_ON = 3000;
+    public static final double SHOOTER_ON = 1;
     public static final double SHOOTER_OFF = 0;
     
-    private static final int ENCODER_TICKS_PER_REV_1 = 180;
-    private static final int ENCODER_TICKS_PER_REV_2 = 180;
-    private final double P_SHOOTER_1 = 1;
-    private final double I_SHOOTER_1 = 0;
-    private final double D_SHOOTER_1 = 0;
-    private final double P_SHOOTER_2 = 1;
-    private final double I_SHOOTER_2 = 0;
-    private final double D_SHOOTER_2 = 0;
     private final int NUM_RETRIES = 10;
-    private PIDConfig firstShooterPIDConfig;
-    private PIDConfig secondShooterPIDConfig;
     
     private double velocity;
     private CANJaguar shooterJaguarFirst;
@@ -27,20 +15,8 @@ public class Shooter extends Subsystem1816 {
     
     public Shooter(int firstShooterJaguarNum, int secondShooterJaguarNum) {
         super("Shooter");
-        shooterJaguarFirst = createCANJaguar(firstShooterJaguarNum,
-                CANJaguar.SpeedReference.kQuadEncoder,
-                ENCODER_TICKS_PER_REV_1,
-                CANJaguar.ControlMode.kSpeed,
-                P_SHOOTER_1, I_SHOOTER_1, D_SHOOTER_1,
-                NUM_RETRIES);
-        shooterJaguarSecond = createCANJaguar(secondShooterJaguarNum,
-                CANJaguar.SpeedReference.kQuadEncoder,
-                ENCODER_TICKS_PER_REV_2,
-                CANJaguar.ControlMode.kSpeed,
-                P_SHOOTER_2, I_SHOOTER_2, D_SHOOTER_2,
-                NUM_RETRIES);
-        firstShooterPIDConfig = PIDTuningManager.getInstance().getPIDConfig("First Shooter");
-        secondShooterPIDConfig = PIDTuningManager.getInstance().getPIDConfig("Second Shooter");
+        shooterJaguarFirst = createCANJaguar(firstShooterJaguarNum, CANJaguar.ControlMode.kPercentVbus, NUM_RETRIES);
+        shooterJaguarSecond = createCANJaguar(secondShooterJaguarNum, CANJaguar.ControlMode.kPercentVbus, NUM_RETRIES);
     }
  
     public void setShooterVelocity(double velocity) {
@@ -76,21 +52,9 @@ public class Shooter extends Subsystem1816 {
         try {
             if(shooterJaguarFirst != null) {
                 shooterJaguarFirst.setX(velocity);
-                //PID tuning code
-                shooterJaguarFirst.setPID(firstShooterPIDConfig.getP(P_SHOOTER_1),
-                        firstShooterPIDConfig.getI(I_SHOOTER_1),
-                        firstShooterPIDConfig.getD(D_SHOOTER_1));
-                firstShooterPIDConfig.setSetpoint(velocity);
-                firstShooterPIDConfig.setValue(shooterJaguarFirst.getSpeed());
             }
             if(shooterJaguarSecond != null) {
                 shooterJaguarSecond.setX(velocity);
-                //PID tuning code
-                shooterJaguarSecond.setPID(secondShooterPIDConfig.getP(P_SHOOTER_2),
-                        secondShooterPIDConfig.getI(I_SHOOTER_2),
-                        secondShooterPIDConfig.getD(D_SHOOTER_2));
-                secondShooterPIDConfig.setSetpoint(velocity);
-                secondShooterPIDConfig.setValue(shooterJaguarSecond.getSpeed());
             }
         }
         catch(Exception e) {
@@ -117,6 +81,27 @@ public class Shooter extends Subsystem1816 {
         }
         catch(Exception e) {
             System.err.println("Failed to create Jaguar.");
+            e.printStackTrace();
+        }
+        return canJaguar;
+    }
+    
+    private CANJaguar createCANJaguar(int id, CANJaguar.ControlMode controlMode, int numRetries){
+        CANJaguar canJaguar = null;
+        try{
+            for(int i = 0; i < numRetries; i++){
+                try {
+                    canJaguar = new CANJaguar(id);
+                    break;
+                }
+                catch(Exception e){
+                    System.out.println("Failed to create CANJaguar");
+                }
+            }
+            canJaguar.changeControlMode(controlMode);
+        }
+        catch(Exception e){
+            System.err.println("Failed to create CANJaguar");
             e.printStackTrace();
         }
         return canJaguar;
