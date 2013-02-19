66ddc38596cff28d1468fd755530c073c2de964f
Srujun Gupta <theununtrium@gmail.com>
2013-02-19 12:26:14 -0600

    Removed PID control from RotateXDegrees Command and added rotation power.
    The rotate command now rotates the robot based on the power factor,
    and does no longer user a PID controller.

--- src/com/edinarobotics/zed/commands/RotateXDegreesCommand.java
+++ src/com/edinarobotics/zed/commands/RotateXDegreesCommand.java
@@ -1,54 +1,51 @@
 package com.edinarobotics.zed.commands;
 
-import com.edinarobotics.utils.pid.PIDConfig;
-import com.edinarobotics.utils.pid.PIDTuningManager;
 import com.edinarobotics.zed.Components;
 import com.edinarobotics.zed.subsystems.DrivetrainRotation;
-import edu.wpi.first.wpilibj.PIDController;
 import edu.wpi.first.wpilibj.command.Command;
 
 public class RotateXDegreesCommand extends Command {
     private DrivetrainRotation drivetrainRotation;
-    private PIDController pidControllerDegrees;
-    private PIDConfig pidConfigDegrees;
     private double degrees;
     
-    private final double P = 1;
-    private final double I = 0;
-    private final double D = 0;
     private final double TIMEOUT_PER_DEGREE = 5;
-    private final double TOLERANCE = 5;
+    private final double ROTATION_POWER = 0.75;
     
     public RotateXDegreesCommand(double degrees) {
         super("RotateXDegrees");
         this.drivetrainRotation = Components.getInstance().drivetrainRotation;
-        this.pidControllerDegrees = new PIDController(P, I, D, drivetrainRotation, drivetrainRotation);
-        this.pidConfigDegrees = PIDTuningManager.getInstance().getPIDConfig("Rotate X Degrees");
         this.degrees = degrees;
         setTimeout(degrees * TIMEOUT_PER_DEGREE);
-        pidControllerDegrees.setAbsoluteTolerance(TOLERANCE);
         requires(drivetrainRotation);
     }
     
     protected void initialize() {
-        pidControllerDegrees.enable();
-        pidControllerDegrees.setSetpoint(degrees);
+        drivetrainRotation.resetGyro();
     }
 
     protected void execute() {
-        //PID tuning code
-        pidControllerDegrees.setPID(pidConfigDegrees.getP(P), pidConfigDegrees.getI(I),
-                pidConfigDegrees.getD(D));
-        pidConfigDegrees.setSetpoint(pidControllerDegrees.getSetpoint());
-        pidConfigDegrees.setValue(drivetrainRotation.pidGet());
+        drivetrainRotation.mecanumPolarRotate(signum(degrees) * ROTATION_POWER);
     }
 
     protected boolean isFinished() {
-        return isTimedOut() || pidControllerDegrees.onTarget();
+        return isTimedOut() || isOnTarget();
+    }
+    
+    private boolean isOnTarget() {
+        return Math.abs(drivetrainRotation.getGyroAngle()) >= Math.abs(degrees);
+    }
+    
+    private byte signum(double number) {
+        if(number > 0) {
+            return 1;
+        } else if(number < 0) {
+            return -1;
+        } else {
+            return 0;
+        }
     }
 
     protected void end() {
-        pidControllerDegrees.disable();
         drivetrainRotation.mecanumPolarRotate(0);
     }
 
