6a1c564a409ffd09d46e32d25d3670abcf58b4ab
Karl Otness <karl@otness.com>
2013-03-06 22:22:59 -0600

    Add Climber subsystem to the project.
    The Climber subsystem provides control of the climber deployment
    servo motor.

---
+++ src/com/edinarobotics/zed/subsystems/Climber.java
@@ -0,0 +1,35 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.edinarobotics.zed.subsystems;
+
+import com.edinarobotics.utils.subsystems.Subsystem1816;
+import edu.wpi.first.wpilibj.Servo;
+
+public class Climber extends Subsystem1816 {
+    private Servo climberServo;
+    private double position;
+    
+    public static final double CLIMBER_UNDEPLOYED = 0;
+    public static final double CLIMBER_DEPLOYED = 1;
+    
+    public Climber(int climberServo){
+        this.climberServo = new Servo(climberServo);
+        position = 0;
+    }
+    
+    public void setClimberDeployed(boolean deployed){
+        setPosition(deployed ? CLIMBER_DEPLOYED : CLIMBER_UNDEPLOYED);
+    }
+    
+    public void setPosition(double position){
+        this.position = position;
+        update();
+    }
+
+    public void update() {
+        climberServo.set(position);
+    }
+}
