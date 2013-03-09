8ee2aeaddf4d8c85b12006b1b67ed09d1301dab8
Karl Otness <karl@otness.com>
2013-03-09 11:12:43 -0600

    Add fixed vision tracking point constants to the fixed point command.
    These constants make it possible to centralize the coordinates used
    for fixed point vision tracking.

--- src/com/edinarobotics/zed/commands/FixedPointVisionTrackingCommand.java
+++ src/com/edinarobotics/zed/commands/FixedPointVisionTrackingCommand.java
@@ -4,6 +4,11 @@ public class FixedPointVisionTrackingCommand extends VisionTrackingCommand {
     private double fixedXSetpoint;
     private double fixedYSetpoint;
     
+    public static final double PYRAMID_BACK_MIDDLE_X = 0.0273556;
+    public static final double PYRAMID_BACK_MIDDLE_Y = -0.29411;
+    public static final double PYRAMID_BACK_RIGHT_X = -0.063829;
+    public static final double PYRAMID_BACK_RIGHT_Y = -0.226891;
+    
     public FixedPointVisionTrackingCommand(double xSetpoint, double ySetpoint) {
         super();
         this.fixedXSetpoint = xSetpoint;
