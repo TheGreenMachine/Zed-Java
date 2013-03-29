c05fab2b8ee44379ec4cbba4e53cfa67b0f91395
Srujun Gupta <theununtrium@gmail.com>
2013-03-29 14:03:25 -0500

    Add extra fixed-point value from practice field testing.

--- src/com/edinarobotics/zed/commands/FixedPointVisionTrackingCommand.java
+++ src/com/edinarobotics/zed/commands/FixedPointVisionTrackingCommand.java
@@ -6,8 +6,10 @@ public class FixedPointVisionTrackingCommand extends VisionTrackingCommand {
     private double fixedXSetpoint;
     private double fixedYSetpoint;
     
-    public static final Point2 PYRAMID_BACK_MIDDLE = new Point2(-0.018, -0.25);
+    public static final Point2 PYRAMID_BACK_MIDDLE_NEW = new Point2(-0.018, -0.25);
     public static final Point2 PYRAMID_BACK_RIGHT = new Point2(-0.063829, -0.226891);
+    public static final Point2 PYRAMID_BACK_MIDDLE = new Point2(0.0273556, -0.29411);
+    public static final Point2 PYRAMID_BACK_MIDDLE_NEW2 = new Point2(-0.143, -0.241);
     
     public FixedPointVisionTrackingCommand(double xSetpoint, double ySetpoint) {
         super();
@@ -23,6 +25,10 @@ public class FixedPointVisionTrackingCommand extends VisionTrackingCommand {
     
     public FixedPointVisionTrackingCommand(Point2 point2) {
         this(point2.getX(), point2.getY());
+    }
+    
+    public FixedPointVisionTrackingCommand(Point2 point2, byte goalType) {
+        this(point2.getX(), point2.getY(), goalType);
     }
     
     protected double getXSetpoint(double distance) {
