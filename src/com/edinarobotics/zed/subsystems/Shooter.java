244e0d0e8e74df8e292e8cf31e6229402c71d671
Karl Otness <karl@otness.com>
2013-04-13 14:53:52 -0500

    Disable shooter to avoid CAN errors.

--- src/com/edinarobotics/zed/subsystems/Shooter.java
+++ src/com/edinarobotics/zed/subsystems/Shooter.java
@@ -10,13 +10,13 @@ public class Shooter extends Subsystem1816 {
     private final int NUM_RETRIES = 10;
     
     private double velocity;
-    private CANJaguar shooterJaguarFirst;
-    private CANJaguar shooterJaguarSecond;
+    //private CANJaguar shooterJaguarFirst;
+    //private CANJaguar shooterJaguarSecond;
     
     public Shooter(int firstShooterJaguarNum, int secondShooterJaguarNum) {
         super("Shooter");
-        shooterJaguarFirst = createCANJaguar(firstShooterJaguarNum, CANJaguar.ControlMode.kPercentVbus, NUM_RETRIES);
-        shooterJaguarSecond = createCANJaguar(secondShooterJaguarNum, CANJaguar.ControlMode.kPercentVbus, NUM_RETRIES);
+        //shooterJaguarFirst = createCANJaguar(firstShooterJaguarNum, CANJaguar.ControlMode.kPercentVbus, NUM_RETRIES);
+        //shooterJaguarSecond = createCANJaguar(secondShooterJaguarNum, CANJaguar.ControlMode.kPercentVbus, NUM_RETRIES);
     }
  
     public void setShooterVelocity(double velocity) {
@@ -25,7 +25,7 @@ public class Shooter extends Subsystem1816 {
     }
     
     public double getFirstJaguarVelocity() {
-        if(shooterJaguarFirst != null) {
+       /* if(shooterJaguarFirst != null) {
             try {
                 return shooterJaguarFirst.getSpeed();
             }
@@ -33,23 +33,24 @@ public class Shooter extends Subsystem1816 {
                 e.printStackTrace();
             }
         }
-        return -1;
+        return -1;*/
+        return 0;
     }
     
     public double getSecondJaguarVelocity() {
-        if(shooterJaguarSecond != null) {
+        /*if(shooterJaguarSecond != null) {
             try {
                 return shooterJaguarSecond.getSpeed();
             }
             catch(Exception e) {
                 e.printStackTrace();
             }
-        }
-        return -1;
+        }*/
+        return 0;
     }
     
     public void update() {
-        try {
+       /* try {
             if(shooterJaguarFirst != null) {
                 shooterJaguarFirst.setX(velocity*0.75);
             }
@@ -60,7 +61,7 @@ public class Shooter extends Subsystem1816 {
         catch(Exception e) {
             System.err.println("Failed to update shooter Jaguars.");
             e.printStackTrace();
-        }
+        }*/
     }
     
     private CANJaguar createCANJaguar(int id, CANJaguar.SpeedReference speedReference,
