16b0404daf1fc8a189e6854d9d5f748b02ca1d77
Karl Otness <karl@otness.com>
2013-04-13 22:16:32 -0500

    Add PrintFormatter to the project.
    PrintFormatter formats LogEvent objects into a format that is
    human-readable and suitable to be printed to a console output.

---
+++ src/com/edinarobotics/utils/log/formatters/PrintFormatter.java
@@ -0,0 +1,37 @@
+package com.edinarobotics.utils.log.formatters;
+
+import com.edinarobotics.utils.log.Formatter;
+import com.edinarobotics.utils.log.LogEvent;
+
+/**
+ * This class implements a Formatter that formats log events into a
+ * human-readable String suitable for printing.
+ */
+public class PrintFormatter implements Formatter {
+    
+    /**
+     * Creates a new PrintFormatter instance.
+     */
+    public PrintFormatter(){
+        
+    }
+    
+    /**
+     * Formats a given LogEvent object into a format suitable for printing.
+     * 
+     * A possible output is:
+     * {@code [SEVERE] _root_.full.logger.name: An error occurred.}
+     * {@code     Exception: If there is an exception. It goes here.}
+     * @param event The LogEvent object to be formatted by this Formatter.
+     * @return A human-readable String representing this LogEvent, that
+     * is suitable for printing.
+     */
+    public String format(LogEvent event){
+        String formattedEvent = "["+event.getLevel().toString().toUpperCase()+"] "
+                +event.getOriginalLoggerName()+": "+event.getMessage();
+        if(event.hasThrowable()){
+            formattedEvent += "\n    "+event.getThrowable().toString();
+        }
+        return formattedEvent;
+    }
+}
