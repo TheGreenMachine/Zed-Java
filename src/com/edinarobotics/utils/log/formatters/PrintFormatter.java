dbb3579277c76ec813a679200c670a1246d959bb
Karl Otness <karl@otness.com>
2013-04-13 23:53:07 -0500

    Change PrintFormatter to use the Logger object stored in LogEvent.

--- src/com/edinarobotics/utils/log/formatters/PrintFormatter.java
+++ src/com/edinarobotics/utils/log/formatters/PrintFormatter.java
@@ -20,15 +20,19 @@ public class PrintFormatter implements Formatter {
      * Formats a given LogEvent object into a format suitable for printing.
      * 
      * A possible output is:
-     * {@code [SEVERE] _root_.full.logger.name: An error occurred.}
+     * {@code [SEVERE] full.logger.name: An error occurred.}
      * {@code     Exception: If there is an exception. It goes here.}
      * @param event The LogEvent object to be formatted by this Formatter.
      * @return A human-readable String representing this LogEvent, that
      * is suitable for printing.
      */
     public String format(LogEvent event){
-        String formattedEvent = "["+event.getLevel().toString().toUpperCase()+"] "
-                +event.getOriginalLoggerName()+": "+event.getMessage();
+        String originLogger = event.getOriginalLogger().getFullNameWithoutRoot();
+        String formattedEvent = "["+event.getLevel().toString().toUpperCase()+"]";
+        if(!originLogger.equals("")){
+            formattedEvent += " "+originLogger;
+        }
+        formattedEvent += ": "+event.getMessage();
         if(event.hasThrowable()){
             formattedEvent += "\n    "+event.getThrowable().toString();
         }
