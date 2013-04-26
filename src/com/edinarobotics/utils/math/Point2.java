1870f9ff8bb0acef4473f6aae7a790a7de5fe8f2
Karl Otness <karl@otness.com>
2013-04-25 23:43:06 -0500

    Add hashCode, equals, and toString methods to Point2.

--- src/com/edinarobotics/utils/math/Point2.java
+++ src/com/edinarobotics/utils/math/Point2.java
@@ -25,6 +25,45 @@ public class Point2 {
     public double getX() {
         return x;
     }
+
+    /**
+     * Returns an {@code int} hash code value for this Point2.
+     * @return An {@code int} hash code value for this Point2.
+     */
+    public int hashCode() {
+        //Auto-generated hashCode method.
+        int hash = 7;
+        hash = 19 * hash + (int) (Double.doubleToLongBits(getX()) ^ (Double.doubleToLongBits(getX()) >>> 32));
+        hash = 19 * hash + (int) (Double.doubleToLongBits(getY()) ^ (Double.doubleToLongBits(getY()) >>> 32));
+        return hash;
+    }
+    
+    /**
+     * Determines whether an Object is equal to this Point2.
+     * 
+     * An object is equal to this Point2 if it is also an instance of Point2
+     * and if it contains equal components.
+     * @param other The object to be tested for equality against this Point2.
+     * @return {@code true} if the objects are equal as defined above
+     * {@code false} otherwise.
+     */
+    public boolean equals(Object other){
+        if(other instanceof Point2){
+            Point2 otherPoint = (Point2)other;
+            return otherPoint.getX() == getX() && otherPoint.getY() == getY();
+        }
+        return false;
+    }
+    
+    /**
+     * Returns a String representation of this Point2.
+     * 
+     * This representation is designed to be human-readable.
+     * @return A human-readable String representation of this Point2.
+     */
+    public String toString(){
+        return "<Point2: ("+getX()+", "+getY()+")>";
+    }
     
     /**
      * Returns the y coordinate of the point
