5fdd85feebe30e1d42c26884eb99b7a2f3e3570b
Srujun Gupta <theununtrium@gmail.com>
2013-05-16 18:10:28 -0500

    Added SpeedControllerMultiplexer implementation for mini-CIMs.
    The Drivetrain class now constructs the new mini-CIMs using the
    multiplexer. The unused encoders were also removed.

--- src/com/edinarobotics/zed/subsystems/Drivetrain.java
+++ src/com/edinarobotics/zed/subsystems/Drivetrain.java
@@ -1,7 +1,10 @@
 package com.edinarobotics.zed.subsystems;
 
+import com.edinarobotics.utils.controllers.SpeedControllerMultiplexer;
 import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.Jaguar;
 import edu.wpi.first.wpilibj.RobotDrive;
+import edu.wpi.first.wpilibj.SpeedController;
 
 public class Drivetrain {
     private final double DISTANCE_PER_PULSE = 1;
@@ -13,21 +16,27 @@ public class Drivetrain {
     private Encoder encoder1;
     private Encoder encoder2;
     
-    public Drivetrain(int frontLeft, int frontRight, int backLeft, int backRight,
-            int encoder1A, int encoder1B, int encoder2A, int encoder2B) {
+    /*
+    public Drivetrain(int frontLeft, int frontRight, int backLeft, int backRight) {
         this.robotDrive = new RobotDrive(frontLeft, backLeft, frontRight, backRight);
         robotDrive.setInvertedMotor(RobotDrive.MotorType.kFrontRight, true);
         robotDrive.setInvertedMotor(RobotDrive.MotorType.kFrontLeft, false);
         robotDrive.setInvertedMotor(RobotDrive.MotorType.kRearRight, true);
         robotDrive.setInvertedMotor(RobotDrive.MotorType.kRearLeft, false);
-        
-        encoder1 = new Encoder(encoder1A, encoder1B);
-        encoder2 = new Encoder(encoder2A, encoder2B);
-        encoder1.setDistancePerPulse(DISTANCE_PER_PULSE);
-        encoder2.setDistancePerPulse(DISTANCE_PER_PULSE);
-        encoder1.start();
-        encoder2.start();
     }
+    */
+    
+    public Drivetrain(int frontLeft, int frontLeftMini, int frontRight, int frontRightMini,
+        int backLeft, int backLeftMini, int backRight, int backRightMini) {
+        this.robotDrive = new RobotDrive(createMultiplexedJaguar(frontLeft, frontLeftMini),
+            createMultiplexedJaguar(backLeft, backLeftMini),
+            createMultiplexedJaguar(frontRight, frontRightMini),
+            createMultiplexedJaguar(backRight, backRightMini));
+        robotDrive.setInvertedMotor(RobotDrive.MotorType.kFrontRight, true);
+        robotDrive.setInvertedMotor(RobotDrive.MotorType.kFrontLeft, false);
+        robotDrive.setInvertedMotor(RobotDrive.MotorType.kRearRight, true);
+        robotDrive.setInvertedMotor(RobotDrive.MotorType.kRearLeft, false);
+     }
     
     public void mecanumPolarStrafe(double magnitude, double direction){
         this.magnitude = magnitude;
@@ -44,11 +53,23 @@ public class Drivetrain {
         robotDrive.mecanumDrive_Polar(magnitude, direction, rotation);
     }
     
+    /**
+     * @deprecated The encoder does not get constructed. Usage will cause exceptions!
+     */
     public double getAverageForwardVelocity() {
         /*
          * The inner 0.5 multiplier is used because mecanum drive only gives
          * a forward velocity of half the wheel's actual rate.
          */
         return ((0.5*encoder1.getRate()) + (0.5*encoder2.getRate()))/2.0;
+    }
+    
+    private SpeedController createMultiplexedJaguar(int id, int idMini){
+        Jaguar[] jaguars = new Jaguar[2];
+        jaguars[0] = new Jaguar(id);
+        jaguars[1] = new Jaguar(idMini);
+        
+        SpeedControllerMultiplexer scMultiplexer = new SpeedControllerMultiplexer(jaguars);
+        return scMultiplexer;
     }
 }
